2345fdbcb2bebc8cf7654373f0cdbd4c2627afe4
Giovanni Biczo <giovanni.biczo@gmail.com>
2013-09-25 23:44:07 +0200

    Corrected Cellhelper::x() and Cellhelper::y() methods, now both return correct values. RDoc documentation added.

--- saxparse1.rb
+++ saxparse1.rb
@@ -6,12 +6,14 @@ require 'fileutils'
 require 'debugger'
 debugger
 class String
+  # Returns true if the given String represents a numeric value 
   def numeric?
     Float(self) != nil rescue false
   end
 end
 
 class Fixnum
+  # Returns the column name corresponding to the given number. e.g: 1.col_name => 'B'
   def col_name
     val=self/26
     (val > 0 ? (val - 1).col_name : "") + (self % 26 + 65).chr
@@ -19,37 +21,61 @@ class Fixnum
 end
 
 class Matrix
+  # Set the cell i,j to x, where i=row, and j=column.
   def []=(i, j, x)
     @rows[i][j]=x
   end
 end
 
+# The Cellhelper module defines some methods useful to manipulate Cell objects
 module Cellhelper
+  # Set the excel cell name (eg: 'A2')
   def r(val); @xlcoords = val; end;
+  # Set cell type    
   def t(val); @type = val; end;
+  # Set cell value
   def v(val); @value = val; end;
+  # Set cell style (number format and style) 
   def s(val); @style = val; end;
   
+  # When called without parameters, returns the x coordinate of the calling cell object based on the value of #@xlcoords
+  # If a parameter is given, #x will return the x coordinate corresponding to the parameter
+  # E.g.: <tt>('B3') # => 1</tt>
   def x(coord=nil)
     if coord.nil?
       coord = @xlcoords
     end
-    ('A'..(coord.scan(/\p{Alpha}+|\p{Digit}+/u)[0])).to_a.length
+    ('A'..(coord.scan(/\p{Alpha}+|\p{Digit}+/u)[0])).to_a.length-1
   end
 
+  # When called without parameters, returns the y coordinate of the calling cell object based on the value of #@xlcoords
+  # If a parameter is given, #y will return the y coordinate corresponding to the parameter
+  # E.g.: <tt>('B3') # => 2</tt>
   def y(coord=nil)
     if coord.nil?
       coord = @xlcoords
     end
-    coord.scan(/\p{Alpha}+|\p{Digit}+/u)[1].to_i
+    coord.scan(/\p{Alpha}+|\p{Digit}+/u)[1].to_i-1
   end
 end
 
+# Simple class representing an excel cell.
 class Cell
   attr_accessor :xlcoords, :type, :value, :comment
   include Cellhelper
 end
 
+##
+# The Xlsheet class is a SAX parser based on the Ox library. It parses a
+# SpreadsheetML (AKA Office Open XML) formatted XML file and returns an array
+# of Cell objects (#cellarray).
+#
+# Xlsheet will omit the following:
+# * empty cells
+# * cells containing formulas
+#
+# Only non-empty cells of merged groupsbe added to #cellarray. A separate array
+# (#mergedcells) is reserved for merging.
 class Xlsheet < ::Ox::Sax
   attr_accessor :xmlstack, :lastopennode, :mergedcells, :cellarray, :cell
   def initialize()
@@ -59,13 +85,16 @@ class Xlsheet < ::Ox::Sax
     @cellarray = []
     @cell = Cell.new
   end
-  
+
+  # Save SAX state-machine state to #xmlstack if and only if the processed
+  # element is a :c (column) or a :mergeCell (merged cell)
   def start_element(name)
     if name == :c || name == :mergeCell
       @xmlstack << name
     end
   end
   
+  # Step back in the stack (#xmlstack.pop), clear actual cell information
   def end_element(name)
     @xmlstack.pop
     if name == :c || name == :mergeCell
@@ -73,6 +102,10 @@ class Xlsheet < ::Ox::Sax
     end
   end
   
+  # Set cell value, style, etc. This will only happen if the cell has an
+  # actual value AND the parser's state is :c.
+  # If the state is :mergeCell AND the actual attribute name is :ref the
+  # attribute will be added to the merged cells array
   def attr(name, str)
     if @xmlstack.last == :c # && name != :s
       @cell.send name, str if @cell.respond_to?(name)
@@ -80,7 +113,9 @@ class Xlsheet < ::Ox::Sax
       @mergedcells << str
     end
   end
-  
+
+  # String type cell content is parsed here. String interpolation using the
+  # sharedStrings.xml file is done in the #Sharedstrings class
   def text(str)
     if @xmlstack.last == :c
       if @cell.type != "shared" && @cell.type != "e" && str.numeric?
@@ -92,17 +127,21 @@ class Xlsheet < ::Ox::Sax
   end
 end
 
+# Ox based SAX parser which pushes shared strings (taken from the sharedString.xml file) to an array 
+# These strings will replace the references in the cells (interpolation). 
 class Sharedstrings < ::Ox::Sax
   attr_accessor :stringarray
   def initialize
     @stringarray=[]
   end
-  
+
+  # Push the comment string into @stringarray
   def text(str)
     @stringarray << str
   end
 end
 
+# The Comments class is a parser which builds an array of comments
 class Comments < ::Ox::Sax
   attr_accessor :commarray, :comment
   def initialize
@@ -110,12 +149,14 @@ class Comments < ::Ox::Sax
     @comment={}
   end
   
+  # Push Cell comment hash (comment + reference) to @commarray
   def text(str)
     @comment[:comment]=str.gsub('&#10;', '')
     @commarray << @comment
     @comment = Hash.new
   end
   
+  # Returns reference
   def attr(name, str)
     if name == :ref
       @comment[:ref]=str
@@ -123,20 +164,40 @@ class Comments < ::Ox::Sax
   end
 end
 
+# The Sheet class represents an excel sheet.
 class Sheet
   attr_accessor :name, :sheetId, :relationId, :data
   def initialize; @data=[]; end
 end
 
+# The Workbook class will open the excel file, and convert it to a collection of
+# Matrix objects
 class Workbook
   include Cellhelper
   attr_accessor :sheetbase, :sheets, :sharedstrings
+  ##
+  # Create a new Workbook object.
+  #
+  # filename is the name of the excel file to be opened (xlsx)
+  #
+  # options is a collection of options that can be passed to Workbook.
+  # Options may include:
+  # * :copymerge (=> true/false) - Copy and repeat the content of the merged cells into the whole group, e.g. 
+  # the group of three merged cells <tt>|   a   |</tt> will become <tt>|a|a|a|</tt>
+  # * :include (Ary) - an array of sheet names to be included
+  # * :exclude (Ary) - an array of sheet names not to be processed
+  #
+  # The excel file is first getting unzipped, then the workbook.xml file gets
+  # processed. This file stores sheet metadata, which will be filtered (by including
+  # and excluding sheets from further processing)
+  #
+  # The next stage is building sheets.
+  # This includes:
+  # * Parsing the XML files representing the sheets
+  # * Interpolation of the shared strings
+  # * adding comments to the cells
+  # * Converting each sheet to a Matrix object
   def initialize(filename, options={})
-    # options is a collection of options that can be passed to Workbook.
-    # options may include:
-    #:copymerge (=> true/false) - merged cell will get the same content instead of being nil or empty
-    #:include (Ary) - an array of sheet names to be included
-    #:exclude (Ary) - an array of sheet names not to be processed
     require 'zip'
     @destination = 'tmp'
     FileUtils.mkdir(@destination)
@@ -179,49 +240,38 @@ class Workbook
   end
   
   private
+  # Given the data found in workbook.xml, create a hash and push it to the sheets
+  # array.
+  #
+  # The hash will not be pushed into the array if the sheet name is blacklisted
+  # (it appears in the *excluded_sheets* array) or does not appear in the list of
+  # included sheets.
+  #
+  # If *included_sheets* (the array of whitelisted sheets) is *nil*, the hash is added.
   def sheetdata included_sheets, excluded_sheets=[]
     @a.locate("workbook/sheets/*").each do |x|
       @sheetbase[:name] = x[:name]
       @sheetbase[:sheetId] = x[:sheetId]
       @sheetbase[:relationId] = x[:"r:id"]
-      @sheets << sheetbase
-      @sheets=
-
-**********************************************-
-a = [:egy, :ketto, :harom]
-b = [{:key1=>:elso, :key2=>:masodik, :key3=>:harmadik}
-  {:key1=>:egy, :key2=>:masodik, :key3=>:harmadik}
-  {:key1=>:elsodik, :key2=>:masodik, :key3=>:harom}] 
-
-a.each do |ex|
-  ef=b.detect{|d| d[:key3]==ex}
-  if !ef.nil?
-    puts "ex: #{ex}, ef:#{ef}"
-  end
-end
-
-**********************************************-
-
-      unless included_sheets.nil?
-        if !(included_sheets.detect{|a| a==@sheetbase[:name]}).nil?
+      i = excluded_sheets.detect{|d| d.to_sym == sheetbase[:name]}
+        if i.nil?
           @sheets << sheetbase
         end
-      else
-        @sheets << sheetbase
-      end
-      unless excluded_sheets.nil?
-        if (excluded_sheets.detect{|a| a==@sheetbase[:name]}).nil?
-          
+        unless included_sheets.nil?
+          included_sheets.each do |incl|
+            if incl[:name] == sheetbase[:name]
+              @sheets << sheetbase
+            end
+          end
+        else
+          @sheets << sheetbase
         end
-      else
-        @sheets << sheetbase
-      end
-
-#      @sheets << sheetbase
-      @sheetbase=Hash.new
+        @sheetbase=Hash.new
     end
   end
   
+  # Build the relationship between sheets and the XML files storing the comments
+  # to the actual sheet.
   def commentsrel #opthash?
     Find.find(@destination + '/xl/worksheets/_rels') do |path|
       if File.basename(path).split(".").last=='rels'
@@ -238,7 +288,8 @@ end
       end
     end
   end
-  
+
+  # Invokes the Sharedstrings helper class
   def shstrings
     strings = Sharedstrings.new()
     File.open(@destination + '/xl/sharedStrings.xml', 'r') do |f|
@@ -247,6 +298,7 @@ end
     @sharedstrings=strings.stringarray
   end
   
+  # Parses the comments related to the actual sheet.  
   def mkcomments(commentfile)
     unless commentfile.nil?
       comms = Comments.new()
@@ -257,16 +309,29 @@ end
     end
   end
 
+  # Returns an array of Matrix objects.
+  # For each sheet, matrixto first checks the address (xlcoords) of the
+  # last cell in the cellarray, then builds a *nil*-filled Matrix object of
+  # size *xlcoords.x, xlcoords.y*.
+  #
+  # The matrix will then be filled with Cell objects according to their coordinates.
+  #
+  # If the *copymerge* parameter is *true*, it creates a submatrix (minor)
+  # of every mergegroup (based on the mergedcells array relative to the actual
+  # sheet), and after the only meaningful cell of the minor is found, it is
+  # copied back to the remaining cells of the group. The coordinates (xlcoords)
+  # of each copied cell is changed to reflect the actual Excel coordinate.
+  # 
+  # The matrix will replace the array of cells in the actual sheet.
   def matrixto(copymerge)
     @sheets.each_with_index do |sheet, i|
-      m=Matrix.build(sheet[:cells].last.y, sheet[:cells].last.x) {nil}#!!!!! each!!! SHEETS or SHEET?
+      m=Matrix.build(sheet[:cells].last.y, sheet[:cells].last.x) {nil}
       sheet[:cells].each do |c|
-        m[c.y-1, c.x-1]=c#.value ##value? c? comment?#
+        m[c.y-1, c.x-1]=c
       end
       if copymerge==true
         sheet[:mergedcells].each do |mc|
           a = mc.split(':')
-          #return matrix cell addresses
           x1=x(a[0])-1 #-1 ?
           y1=y(a[0])-1
           x2=x(a[1])-1 #-1 ?
