c0b04450b44b74d4a4120682a02faf84a5f750d8
Giovanni Biczo <giovanni.biczo@gmail.com>
2013-11-25 00:06:38 +0100

    Code cleanup. TODO: split format string (;)? cellhelper.rb to be cleaned up

--- lib/oxcelix/numformats.rb
+++ lib/oxcelix/numformats.rb
@@ -1,26 +1,14 @@
-
 module Oxcelix
   # The Cellhelper module defines some methods useful to manipulate Cell objects
   module Numformats
     attr_accessor :dtmap
     def initialize
-      @dtmap = {'hh.mm'=>'%H.%M', 'hh.m'=>'%H.%-M', 'H.mm'=>'%-k.%M', 'H.m'=>'%-k.%-M', 'mm.ss'=>'%M.%-S', 'm.ss'=>'%-M.%-S', \
-          'mm.s'=>'%M.%S', 'm.s'=>'%-M.%S', 'H'=>'%-k', 'hh'=>'%H', 'm{5}'=>'%b', 'm{4}'=>'%B', 'm{3}'=>'%b', 'mm'=>'%m', \
-          'm'=>'%-m', 'd{4}'=>'%A', 'd{3}}'=>'%a', 'dd'=>'%d', 'd'=>'%-d', 'y{4}'=>'%Y', 'yy'=>'%y', 'AM/PM'=>'%p', \
-          'A/P'=>'%p', '.0'=>'', 's'=>'%S', 'ss'=>'%-S'}
-      @dtmap = {'hh'=>'%H', 'ii'=>'%M', 'i'=>'%-M', 'H'=>'%-k', 'ss'=>'%-S', 's'=>'%S', 'm{5}'=>'%b', 'm{4}'=>'%B',
-                'm{3}'=>'%b', 'mm'=>'%m', 'm'=>'%-m', 'd{4}'=>'%A', 'd{3}'=>'%a', 'dd'=>'%d', 'd'=>'%-d', 'yyyy'=>'%Y',
-                'yy'=>'%y', 'AM/PM'=>'%p', 'A/P'=>'%p', '.0'=>'', 's'=>'%S', 'ss'=>'%-S'}
       @dtmap = {'hh'=>'%H', 'ii'=>'%M', 'i'=>'%-M', 'H'=>'%-k', \
                     'ss'=>'%-S', 's'=>'%S', 'mmmmm'=>'%b', 'mmmm'=>'%B', 'mmmm'=>'%b', 'mm'=>'%m', \
                     'm'=>'%-m', 'dddd'=>'%A', 'ddd'=>'%a', 'dd'=>'%d', 'd'=>'%-d', 'yyyy'=>'%Y', \
                     'yy'=>'%y', 'AM/PM'=>'%p', 'A/P'=>'%p', '.0'=>'', 'ss'=>'%-S', 's'=>'%S'}
-    ##?????
     end
 
-    'hh.mm'=>'%H.%M', 'hh.m'=>'%H.%-M', 'H.mm'=>'%-k.%M', 'H.m'=>'%-k.%-M', 'mm.ss'=>'%M.%-S', 'm.ss'=>'%-M.%-S', 'mm.s'=>'%M.%S', 'm.s'=>'%-M.%S', 'H'=>'%-k', 'hh'=>'%H', 'm{5}'=>'%b', 'm{4}'=>'%B', 'm{3}'=>'%b', 'mm'=>'%m', 'm'=>'%-m', 'd{4}'=>'%A', 'd{3}}'=>'%a', 'dd'=>'%d', 'd'=>'%-d', 'y{4}'=>'%Y', 'yy'=>'%y', 'AM/PM'=>'%p', 'A/P'=>'%p', '.0'=>'', 's'=>'%S', 'ss'=>'%-S'
-
-#    fhash={:rvalue=>nil, :dvalue=>nil}
 SPRINTF vagy FORMAT!!! (Kernel::)
 1) 4 csoportra osztas:
         1) Format for positive numbers
@@ -74,93 +62,6 @@ def numeric value, fmt
   return ostring
 end
 
-  def vgrp str
-    if !str.size == 0
-      if eval(str) == nil #hashes
-        return ("#" + str.size.to_s) #nem jo, mindig oda fogja tenni!!!!!! vissza az eredeti fuggvenybe!!!!
-      elsif eval(str) == 0 #zeroes
-        return str.size.to_s
-      end
-    else
-      return nil
-    end
-  end
-
-
-
-
-
-  #grp=0
-  hashes = 0
-  zeroes = 0
-
-  arr = fmt.split(".")
-  arr.each_with_index do |f,j|
-    f.chars.each_with_index do |ch, i|
-      if ch == '#'
-        hashes += 1
-      elsif ch == '0'
-        zeroes += 1
-      elsif ch == '.'
-        ostring = mkstr(ostring, hashes, zeroes, ch)
-        hashes = 0
-        zeroes = 0
-      elsif i == 0
-        ostring.prepend ch + ' '
-      elsif
-        i == f.chars.size - 1
-        ostring += ch
-      end
-    end
-    if j==0 || arr.size > 1
-      ostring += "."
-    end
-    ostring = mkstr(ostring, hashes, zeroes, '')
-  end
-end
-  #if grp <= 1
-  #formatted_n = sprintf('%2.2f $', n).to_s.reverse.gsub(/\d#{grp}(?=\d)/, '\&').reverse ####ATIRANDUSZ
-
-  def mkstr (ostring, hashes, zeroes, ch)
-    if hashes > 0 && !ostring.include?(".")
-      ostring += '#' + hashes.to_s  #nem jo! ketszer fogja a hashmarkot beletenni 
-    end
-    if zeroes > 0
-      ostring += zeroes.to_s
-    end
-    if ostring.include?(".") && !ostring.include?("e")
-      ostring += "f"
-    elsif ostring.include?(".") && ostring.include?("e")
-      ostring += "e"
-    else
-      ostring += "d"
-    end
-    ostring += ch
-  end
-
-
-
-  fmtarr = fmt.split('.')
-  t = ''
-  if fmtarr.size > 1
-    t = 'f'
-  else
-    t = 'd'
-  end
-  hmark = 0
-  zeroes = 0
-  format = '%'
-  fmtarr.eack do |x|
-    x.scan(/#/) { hmark += 1}
-    x.scan(/0/) { zeroes += 1}
-  if zeroes <= 0
-    zeroes = ''
-  end
-  format += zeroes.to_s + hmark.to_s
-  #e, %, $, vesszo stb.
-
-end
-
 def datetime value, fmt
   deminutified = fmt.gsub(/(?<hrs>H|h)(?<div>.)m/, '\k<hrs>\k<div>i')
                      .gsub(/im/, 'ii')
@@ -169,40 +70,8 @@ def datetime value, fmt
   rubified = deminutified.gsub /[yMmDdHhSsi]/, @dtmap
 end
 
- 
-    FARY=[
-    #    ID  Format Code
-    { :rvalue => Proc.new{@value}, :dvalue => Proc.new{@value} }, #0   General 
-    { :rvalue => Proc.new{@value}, :dvalue => Proc.new{@value.to_i} }, #1   0 
-    { :rvalue => Proc.new{@value.to_f}, :dvalue => Proc.new{@value.to_f} m[c.y, c.x]=c}, #2   0.00 
-    { :rvalue => Proc.new{@value}, :dvalue => Proc.new{@value.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} },#3   #,##0
-    { :rvalue => Proc.new{@value.to_f}, :dvalue => Proc.new{@value.to_f.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} }, #4   #,##0.00
-    nil, nil, nil, nil, #5, 6, 7, 8
-    { :rvalue => Proc.new{@value.to_i}, :dvalue => Proc.new{("%2.2i\%" % @value)} }, #9   0% ##*100?
-    { :rvalue => Proc.new{@value.to_f}, :dvalue => Proc.new{("%2.2f\%" % @value)} }, #10  0.00%
-    { :rvalue => Proc.new{@value.to_f}, :dvalue => Proc.new{("%E" % @value)} }, #11  0.00E+00
-12  # ?/? RATIONAL?
-13  # ??/?? RATIONAL?
-    { :rvalue => Proc.new{DateTime.new(1899,12,30)+@value}, :dvalue => Proc.new{(DateTime.new(1899,12,30)+@value).strftime("%d/%m/%Y")} }, #14  d/m/yyyy
-    { :rvalue => Proc.new{DateTime.new(1899,12,30)+@value}, :dvalue => Proc.new{(DateTime.new(1899,12,30)+@value).strftime("%d-%b-%y")} }, #15  d-mmm-yy
-    { :rvalue => Proc.new{DateTime.new(1899,12,30)+@value}, :dvalue => Proc.new{(DateTime.new(1899,12,30)+@value).strftime("%d-%b")} }, #16  d-mmm
-17  { :rvalue => Proc.new{DateTime.new(1899,12,30)+@value}, :dvalue => Proc.new{(DateTime.new(1899,12,30)+@value).strftime("%b-%y")} }, #mmm-yy
-18  h:mm tt
-19  h:mm:ss tt
-20  H:mm
-21  H:mm:ss
-22  m/d/yyyy H  #%m/,##0 ;(#,##0)
-nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil, #23-37
-38  #,##0 ;[Red](#,##0)
-39  #,##0.00;(#,##0.00)
-40  #,##0.00;[Red](#,##0.00)
-45  mm:ss
-46  [h]:mm:ss
-47  mmss.0
-48  ##0.0E+0
-49  @ ]
 
-  end
+end
   module Cellvalues
     # Set the excel cell name (eg: 'A2')
     # @param [String] val Excel cell address name
