b1c90037148ab829aa8aa5c3e6c0cf1de14af8e4
Giovanni Biczo <gbiczo@users.noreply.github.com>
2013-12-06 19:03:58 +0100

    Added to_ru and to_fmt instance methods to the Sheet class. Cleaned up to_m. Documentation added.

--- lib/oxcelix/sheet.rb
+++ lib/oxcelix/sheet.rb
@@ -3,6 +3,7 @@ module Oxcelix
   # The Sheet class represents an excel sheet.
   class Sheet < Matrix
     include Cellhelper
+    include Numberhelper
   # @!attribute [rw] name
   #   @return [String] Sheet name
   # @!attribute [rw] sheetId
@@ -25,15 +26,50 @@ module Oxcelix
         super(i,j[0])
       end
     end
-
+    
+    #The to_m method returns a simple Matrix object filled with the raw values of the original Sheet object.
+    # @return [Matrix] a collection of string values (the former #Cell::value)
     def to_m(*attrs)
-      m=Matrix.build(self.col(0).length, self.row(0).length){nil}
-      self.each do |x, row, col|
+      m=Matrix.build(self.row_size, self.column_size){nil}
+      self.each_with_index do |x, row, col|
         if attrs.size == 0 || attrs.nil?
-          m[row, col]=x.value
+          m[row, col] = x.value
         end
       end
       return m
     end
-  end
+    
+    # The to_ru method returns a Matrix of "rubified" values. It basically builds a new Matrix
+    # and puts the result of the #Cell::to_ru method of every cell of the original sheet in
+    # the corresponding Matrix cell.
+    # @return [Matrix] a collection of ruby objects (#Integers, #Floats, #DateTimes, #Rationals, #Strings)
+    def to_ru
+      m=Matrix.build(self.row_size, self.column_size){nil}
+      self.each_with_index do |x, row, col|
+        if x.nil?
+          m[row, col] = nil
+        else
+          m[row, col] = x.to_ru
+        end
+      end
+      return m
+    end
+    
+    # The to_fmt method returns a Matrix of "formatted" values. It basically builds a new Matrix
+    # and puts the result of the #Cell::to_fmt method of every cell of the original sheet in
+    # the corresponding Matrix cell. The #Cell::to_fmt will pass the original values to to_ru, and then
+    # depending on the value, will run strftime on DateTime objects and sprintf on numeric types.
+    # @return [Matrix] a collection of Strings
+    def to_fmt
+      m=Matrix.build(self.row_size, self.column_size){nil}
+      self.each_with_index do |x, row, col|
+        if x.nil?
+          m[row, col] = nil
+        else
+          m[row, col] = x.to_fmt
+        end
+     end
+     return m
+   end
+ end
 end
