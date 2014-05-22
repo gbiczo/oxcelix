803514d26609a64758417fbcd923f4a46ab7f3ff
Giovanni Biczo <gbiczo@users.noreply.github.com>
2014-05-22 12:00:01 +0200

    attr method copied in

--- lib/oxcelix/sax/rang_xlsheet.rb
+++ lib/oxcelix/sax/rang_xlsheet.rb
@@ -1,10 +1,21 @@
 module Oxcelix
   class Paginatedsheet < Xlsheet
     attr_accessor :xmlstack, :mergedcells, :cellarray, :cell
+    
     def initialize(per_page, pageno)
       @PER_PAGE=per_page
       @PAGENO=pageno
       super
     end
+    
+    def attr(name, str)
+      # if PER_PAGE?
+      if @xmlstack.last == :c
+        @cell.send name, str if @cell.respond_to?(name)
+      elsif xmlstack.last == :mergeCell && name == :ref
+        @mergedcells << str
+      end
+    end
+    
   end
 end
