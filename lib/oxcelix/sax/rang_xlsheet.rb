1f9c74d961ebe7128ecfcabaa03577810a3133db
Giovanni Biczo <gbiczo@users.noreply.github.com>
2014-05-22 11:37:34 +0200

    Initialize done

--- lib/oxcelix/sax/rang_xlsheet.rb
+++ lib/oxcelix/sax/rang_xlsheet.rb
@@ -1,4 +1,10 @@
 module Oxcelix
   class Paginatedsheet < Xlsheet
+    attr_accessor :xmlstack, :mergedcells, :cellarray, :cell
+    def initialize(per_page, pageno)
+      @PER_PAGE=per_page
+      @PAGENO=pageno
+      super
+    end
   end
 end
